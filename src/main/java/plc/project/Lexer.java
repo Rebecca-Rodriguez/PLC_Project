81cbbb471a7e06e628939b45c2fa892cf712f067
Rebecca <contact@rebeccavelez.dev>
2022-09-17 17:18:56 -0400

    Updated peak and lexIdentifier

--- src/main/java/plc/project/Lexer.java
+++ src/main/java/plc/project/Lexer.java
@@ -1,82 +1,106 @@
 package plc.project;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
+
 /**
  * The lexer works through three main functions:
- *
- *  - {@link #lex()}, which repeatedly calls lexToken() and skips whitespace
- *  - {@link #lexToken()}, which lexes the next token
- *  - {@link CharStream}, which manages the state of the lexer and literals
- *
+ * <p>
+ * - {@link #lex()}, which repeatedly calls lexToken() and skips whitespace
+ * - {@link #lexToken()}, which lexes the next token
+ * - {@link CharStream}, which manages the state of the lexer and literals
+ * <p>
  * If the lexer fails to parse something (such as an unterminated string) you
  * should throw a {@link ParseException} with an index at the character which is
  * invalid or missing.
- *
+ * <p>
  * The {@link #peek(String...)} and {@link #match(String...)} functions are
  * helpers you need to use, they will make the implementation a lot easier.
  */
 public final class Lexer {
     private final CharStream chars;
+
     public Lexer(String input) {
         chars = new CharStream(input);
     }
+
+
     /**
      * Repeatedly lexes the input using {@link #lexToken()}, also skipping over
      * whitespace where appropriate.
      */
     public List<Token> lex() {
-        System.out.println("Hello this is a test by Becca!");
-
         throw new UnsupportedOperationException(); //TODO
     }
+
     /**
      * This method determines the type of the next token, delegating to the
      * appropriate lex method. As such, it is best for this method to not change
      * the state of the char stream (thus, use peek not match).
-     *
+     * <p>
      * The next character should start a valid token since whitespace is handled
      * by {@link #lex()}
      */
     public Token lexToken() {
         throw new UnsupportedOperationException(); //TODO
     }
+
     public Token lexIdentifier() {
-        throw new UnsupportedOperationException(); //TODO
+        match("[A-Za-z0-9_]");
+        while (match("[A-Za-z0-9_-]"));
+        return chars.emit(Token.Type.IDENTIFIER);
     }
+
     public Token lexNumber() {
         throw new UnsupportedOperationException(); //TODO
     }
+
+    // BECCA
     public Token lexCharacter() {
         throw new UnsupportedOperationException(); //TODO
     }
+
     public Token lexString() {
         throw new UnsupportedOperationException(); //TODO
     }
+
     public void lexEscape() {
         throw new UnsupportedOperationException(); //TODO
     }
+
     public Token lexOperator() {
         throw new UnsupportedOperationException(); //TODO
     }
+
     /**
      * Returns true if the next sequence of characters match the given patterns,
      * which should be a regex. For example, {@code peek("a", "b", "c")} would
      * return true if the next characters are {@code 'a', 'b', 'c'}.
      */
     public boolean peek(String... patterns) {
-        throw new UnsupportedOperationException(); //TODO (in lecture)
+        throw new UnsupportedOperationException(); //TODO
     }
+
     /**
      * Returns true in the same way as {@link #peek(String...)}, but also
      * advances the character stream past all matched characters if peek returns
      * true. Hint - it's easiest to have this method simply call peek.
      */
     public boolean match(String... patterns) {
-        throw new UnsupportedOperationException(); //TODO (in lecture)
+        boolean peak = peek(patterns);
+        if (peak) {
+            for (int i = 0; i < patterns.length; i++) {
+                chars.advance();
+            }
+        }
+        return peak;
     }
+
     /**
      * A helper class maintaining the input string, current index of the char
      * stream, and the current length of the token being matched.
-     *
+     * <p>
      * You should rely on peek/match for state management in nearly all cases.
      * The only field you need to access is {@link #index} for any {@link
      * ParseException} which is thrown.
@@ -85,22 +109,28 @@ public final class Lexer {
         private final String input;
         private int index = 0;
         private int length = 0;
+
         public CharStream(String input) {
             this.input = input;
         }
+
         public boolean has(int offset) {
             return index + offset < input.length();
         }
+
         public char get(int offset) {
             return input.charAt(index + offset);
         }
+
         public void advance() {
             index++;
             length++;
         }
+
         public void skip() {
             length = 0;
         }
+
         public Token emit(Token.Type type) {
             int start = index - length;
             skip();
